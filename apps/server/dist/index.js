c8e37bc150715b1d6cc58a55fc449cb6602e2fb0
Fynn Emmanuel Junior <fynn.emmanuel.junior@gmail.com>
2023-11-29 09:26:47 +0000

    update on index.ts

--- apps/server/dist/index.js
+++ apps/server/dist/index.js
@@ -1,16 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const database = process.env.DATABASE_URI;
 const app = (0, express_1.default)();
-const PORT = 3500;
-app.get('/', (req, res) => {
-    res.json('welcome to express and node js crash course');
-});
-app.get('/hi', (req, res) => {
-    res.send('hi hello');
-    console.log('we good');
-});
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const PORT = process.env.PORT || 3500;
+// middlewwares
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+//Routes
+app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongoose_1.default.connect(database)
+        .then(() => console.log(`server running on port ${PORT}`))
+        .catch((err) => console.log(`database connectivity failed: ${err.message}`));
+}));
--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,17 +1,28 @@
 import express,{Request,Response} from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const database = process.env.DATABASE_URI   
 
 const app = express()
 
-const PORT = 3500;
+const PORT = process.env.PORT ||  3500;
 
-app.get('/',(req:Request,res:Response) => {
-    res.json('welcome to express and node js crash course')
+// middlewwares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
+
+//Routes
+ 
+
+
+app.listen(PORT, async () => {
+    await mongoose.connect(database)
+        .then(() => console.log(`server running on port ${PORT}`))
+        .catch((err) => console.log(`database connectivity failed: ${err.message}`))
 })
-
-app.get('/hi', (req:Request,res:Response) => {
-    res.send('hi hello')   
-    console.log('we good')
-})
-
-
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
